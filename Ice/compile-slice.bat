853e2cbcd8b38e8491dd455b1d905063c16c32ea
Peter C. Chapin <PChapin@vtc.vsc.edu>
2015-12-27 20:02:32 -0500

    Updated documentation on setting up Ice, wxWidgets, Boost, and Threadpool.

--- Ice/compile-slice.bat
+++ Ice/compile-slice.bat
@@ -7,13 +7,13 @@ IF [%1]==[-q] SET QUIET=1
 IF [%1]==[--quiet] SET QUIET=1
 
 :: Check for empty variable and warn if it's not set.
-IF [x%ICEROOT%]==[x] ECHO WARNING: ICEROOT is unset. Please set it in your system environment variables.
+IF [x%ICE_HOME%]==[x] ECHO WARNING: ICE_HOME is unset. Please set it in your system environment variables.
 
 :: Location of the slice compiler.
-SET SLICE_COMPILER=%ICEROOT%\bin\slice2cpp
+SET SLICE_COMPILER=%ICE_HOME%\bin\slice2cpp
 
 :: Directory where the *.ice files reside.
 SET SLICE_DIR=.
 
 :: Loop through the directory which has the slice code in it and compile every Ice file.
-FOR /F %%a IN ('dir /b %SLICE_DIR%\*.ice') DO %SLICE_COMPILER% --stream -I%SLICE_DIR% -I%ICEROOT%\slice --output-dir=%SLICE_DIR% %SLICE_DIR%\%%a && IF [%QUIET%]==[0] echo Success: %%a || echo Failure: %%a
+FOR /F %%a IN ('dir /b %SLICE_DIR%\*.ice') DO %SLICE_COMPILER% --stream -I%SLICE_DIR% -I%ICE_HOME%\slice --output-dir=%SLICE_DIR% %SLICE_DIR%\%%a && IF [%QUIET%]==[0] echo Success: %%a || echo Failure: %%a
--- Ice/compile-slice.sh
+++ Ice/compile-slice.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Check for empty variable and warn if it's not set.
-if [ "$ICEROOT" = "" ]; then
-    echo "Error: ICEROOT environment variable is unset."
+if [ "$ICE_HOME" = "" ]; then
+    echo "Error: ICE_HOME environment variable is unset."
     exit 1
 fi
 
 # Location of the slice compiler.
-SLICE_COMPILER=$ICEROOT/bin/slice2cpp
+SLICE_COMPILER=$ICE_HOME/bin/slice2cpp
 
 # Directory where the *.ice files reside.
 SLICE_DIR=.
 
 # Loop through the directory which has the slice code in it and compile every Ice file.
 for FILE in $SLICE_DIR/*.ice; do
-    if $SLICE_COMPILER -I$SLICE_DIR -I$ICEROOT/slice --output-dir=$SLICE_DIR $FILE; then
+    if $SLICE_COMPILER -I$SLICE_DIR -I$ICE_HOME/slice --output-dir=$SLICE_DIR $FILE; then
         echo "Success: $FILE"
     else
         echo "Failure: $FILE"
